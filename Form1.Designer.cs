570e7cbbdcacf0456ee136a1f3d5d2388ba04a35
antipups <nikitakurkurin@protonmail.ch>
2020-03-11 19:09:07 +0300

    Первая неудачная кнопка.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace second_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DO
+            IT! = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DO IT!
+            // 
+            this.DO
+            IT!.Location = new System.Drawing.Point(12, 11);
+            this.DO
+            IT!.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DO
+            IT!.Name = "DO IT!";
+            this.DO
+            IT!.Size = new System.Drawing.Size(74, 42);
+            this.DO
+            IT!.TabIndex = 0;
+            this.DO
+            IT!.Text = "DO IT!";
+            this.DO
+            IT!.UseVisualStyleBackColor = true;
+            this.DO
+            IT!.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.DO
+            IT!)
+            ;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
