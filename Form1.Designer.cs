bab5b8ca16f8319e7249eab7ee302d4f5d29d585
antipups <nick.kurkurin@protonmail.com>
2020-04-15 13:41:07 +0300

    Сделано недорисование.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace second_lab
+﻿using System.Windows.Forms;
+
+namespace second_lab
 {
     partial class Form1
     {
@@ -30,9 +32,19 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -40,50 +52,119 @@
             this.button1.Location = new System.Drawing.Point(12, 9);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 34);
+            this.button1.Size = new System.Drawing.Size(180, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "sex";
+            this.button1.Text = "Загрузить картинку";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(106, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 34);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(198, 37);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(262, 231);
+            this.pictureBox1.Size = new System.Drawing.Size(767, 744);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1065, 37);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(767, 744);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // this.pictureBox2.MouseClick += new MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseDown += new MouseEventHandler(this.pictureBox2_MouseMove);
+            // this.pictureBox2.MouseWheel += new MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 75);
+            this.trackBar1.Maximum = 254;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(180, 56);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 149);
+            this.trackBar2.Maximum = 254;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(180, 56);
+            this.trackBar2.TabIndex = 5;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(12, 221);
+            this.trackBar3.Maximum = 254;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(180, 56);
+            this.trackBar3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Красный";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Синий";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Зелёный";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 826);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Лабораторная_работа_№2";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.trackBar3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace second_lab
 {
     public partial class Form1 : Form
     {
+        private bool click = false;
         public Form1()
         {
             InitializeComponent();
@@ -20,46 +21,58 @@ namespace second_lab
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Image image;
             OpenFileDialog openDialog = new OpenFileDialog();
             openDialog.Filter = @"Файлы изображений|*.bmp;*.png;*.jpg";
             if (openDialog.ShowDialog() != DialogResult.OK)
                 return;
             
-            Image bmp = Image.FromFile(@"C:\Users\kurku\Documents\C#Projects\second_lab_C#\second_lab\1.jpg");
-            Graphics gr = Graphics.FromImage(bmp);
-            gr.DrawLine(Pens.Black, 1, 1, 100, 100);
-            gr.Save();
-            pictureBox1.Image = bmp;
-
-            // PixelFormat pf = img.PixelFormat;
-            // pf.
-            // try
-            // {
-            //     image = Image.FromFile(openDialog.FileName);
-            // }
-            // catch (OutOfMemoryException ex)
-            // {
-            //     MessageBox.Show(@"Ошибка чтения картинки");
-            //     return;
-            // }
-            //
-            // pictureBox1.Image = image;
-            // pictureBox1.AutoSize = true;
-
+            Image first, bmp;
+            try
+            {
+                first = Image.FromFile(openDialog.InitialDirectory + openDialog.FileName);
+                bmp = Image.FromFile(openDialog.InitialDirectory + openDialog.FileName);
+            }
+            catch (OutOfMemoryException ex)
+            {
+                MessageBox.Show(@"Ошибка чтения картинки");
+                return;
+            }
+            
+            pictureBox1.Width = 500;
+            pictureBox1.Height = 500;
+            pictureBox1.Image = first;
+            pictureBox2.Width = 500;
+            pictureBox2.Height = 500;
+            pictureBox2.Image = bmp;
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
         {
-            // throw new System.NotImplementedException();
+            Graphics gr = Graphics.FromImage(pictureBox2.Image);
+            int i = 0;
+            Random rnd = new Random();
+            while (i < 50)
+            {
+                int value = rnd.Next(-25, 25);
+                int value2 = rnd.Next(-25, 25);
+                button1.Text = value.ToString();
+                gr.DrawLine(new Pen(Color.FromArgb(rnd.Next(0, 254), trackBar1.Value, trackBar2.Value, trackBar3.Value), 2), e.X, e.Y, e.X + value, e.Y + value2);
+                i++;
+            }
+            gr.Save();
+            pictureBox2.Refresh();
         }
 
-        
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
             // throw new System.NotImplementedException();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
